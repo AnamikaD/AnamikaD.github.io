49a0eeb089a9e6ddbbd0b8558ac82b7ebbe1f8a6
AnamikaD <das.anamika339@gmail.com>
2017-08-20 16:30:15 +0530

    Updates

--- scripts/Auto_gen_1.sh
+++ scripts/Auto_gen_1.sh
@@ -2,5 +2,6 @@
 # Go to sg-events directory
 cd /home/ubuntu/sg-events 
 npm install
-npm i node-github 
+npm i node-github
+export SHEET_ID='1vBP7CnFS7tl1lvSZtZ9X-Wy9UdfHOCjwKKHt-mf8Po0'; 
 NODE_GITHUB_TOKEN=<token> node index.js
